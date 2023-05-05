4a3cc3299b3f97bf857cab3c6f3cfe22c0b5c58a
Thanyapisit Buaprakong <nyman3527@gmail.com>
2023-05-06 00:11:47 +0700

    add latest

---
+++ loadtest.js
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+    vus: 10, // number of virtual users
+    duration: '30s', // test duration
+};
+
+export default function () {
+    const res = http.get('http://40.119.241.117');
+
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+        'has crocodiles': (r) => r.json('crocodiles.length') > 0,
+    });
+
+    sleep(1);
+}
