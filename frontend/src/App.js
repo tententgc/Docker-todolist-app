04325ad40351c3aa442798c09f62f3f03d7ab1a1
Thanyapisit Buaprakong <nyman3527@gmail.com>
2023-05-07 00:38:54 +0700

    fix config env bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,77 +1,78 @@
 import { useState, useEffect } from 'react';
 import axios from "axios";
-
 import './app.css';
 import Item from './components/Item'
 
+
+
 function App() {
 
-  const [text, setText] = useState("");
-  const [todo, setTodo] = useState([]);
-  const [isUpdating, setUpdating] = useState("");
+    const [text, setText] = useState("");
+    const [todo, setTodo] = useState([]);
+    const [isUpdating, setUpdating] = useState("");
 
-  useEffect(() => {
-    axios.get(`${process.env.REACT_APP_API_BASE_URL}/get-todo`)
-      .then((res) => setTodo(res.data))
-      .catch((err) => console.log(err));
-  }, []);
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_API_BASE_URL}/get-todo`)
+            .then((res) => setTodo(res.data))
+            .catch((err) => console.log(err));
+    }, []);
 
-  const addUpdateTodo = () => {
+    const addUpdateTodo = () => {
 
-    if (isUpdating === "") {
-      axios.post(`${process.env.REACT_APP_API_BASE_URL}/save-todo`, { text })
-        .then((res) => {
-          console.log(res.data);
-          setText("");
-        })
-        .catch((err) => console.log(err));
-    } else {
-      axios.post(`${process.env.REACT_APP_API_BASE_URL}/update-todo`, { _id: isUpdating, text })
-        .then((res) => {
-          console.log(res.data);
-          setText("");
-          setUpdating("");
-        })
-        .catch((err) => console.log(err));
+        if (isUpdating === "") {
+            axios.post(`${process.env.REACT_APP_API_BASE_URL}/save-todo`, { text })
+                .then((res) => {
+                    console.log(res.data);
+                    setText("");
+                })
+                .catch((err) => console.log(err));
+        } else {
+            axios.post(`${process.env.REACT_APP_API_BASE_URL}/update-todo`, { _id: isUpdating, text })
+                .then((res) => {
+                    console.log(res.data);
+                    setText("");
+                    setUpdating("");
+                })
+                .catch((err) => console.log(err));
+        }
     }
-  }
 
-  const deleteTodo = (_id) => {
-    axios.post(`${process.env.REACT_APP_API_BASE_URL}/delete-todo`, { _id })
-      .then((res) => console.log(res.data))
-      .catch((err) => console.log(err));
-  }
+    const deleteTodo = (_id) => {
+        axios.post(`${process.env.REACT_APP_API_BASE_URL}/delete-todo`, { _id })
+            .then((res) => console.log(res.data))
+            .catch((err) => console.log(err));
+    }
 
-  const updateTodo = (_id, text) => {
-    setUpdating(_id);
-    setText(text);
-  }
+    const updateTodo = (_id, text) => {
+        setUpdating(_id);
+        setText(text);
+    }
 
-  return (
-    <div className="App">
-      <div className="container">
-        <h1>ToDo App</h1>
-        <div className="top">
-          <input
-            type="text"
-            placeholder='Write Something...'
-            value={text}
-            onChange={(e) => setText(e.target.value)} />
-          <div className="add"
-            onClick={addUpdateTodo}>{isUpdating ? "Update" : "Add"}</div>
+    return (
+        <div className="App">
+            <div className="container">
+                <h1>ToDo App</h1>
+                <div className="top">
+                    <input
+                        type="text"
+                        placeholder='Write Something...'
+                        value={text}
+                        onChange={(e) => setText(e.target.value)} />
+                    <div className="add"
+                        onClick={addUpdateTodo}>{isUpdating ? "Update" : "Add"}</div>
+                </div>
+
+                <div className="list">
+                    {todo.map(item => <Item
+                        key={item._id}
+                        text={item.text}
+                        remove={() => deleteTodo(item._id)}
+                        update={() => updateTodo(item._id, item.text)} />)}
+                </div>
+
+            </div>
         </div>
-
-        <div className="list">
-          {todo.map(item => <Item
-            key={item._id}
-            text={item.text}
-            remove={() => deleteTodo(item._id)}
-            update={() => updateTodo(item._id, item.text)} />)}
-        </div>
-
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
--- frontend/src/test.js
+++ frontend/src/test.js
@@ -1,1 +1,2 @@
+require('dotenv').config()
 console.log(`${process.env.REACT_APP_API_BASE_URL}/get-todo`)
